f120c23657b869e3fc9a09803ecac93fd25ed242
Anthuan Vasquez <me@anthuanvasquez.net>
2018-01-06 02:03:12 -0400

    Fixes

--- includes/general.php
+++ includes/general.php
@@ -1,60 +1,65 @@
 <?php
 
-add_action( 'admin_init', 'add_meta_boxes' );
 function add_meta_boxes() {
-    add_meta_box( 'some_metabox', 'Anime Relationship', 'anime_field', 'anime_episode' );
+	add_meta_box( 'anime-metabox', __( 'Anime Relationship', 'anva-anime-manager' ), 'anime_field', 'anime_episode' );
 }
+add_action( 'admin_init', 'add_meta_boxes' );
 
 function anime_field() {
-    global $post;
-    $selected_anime = get_post_meta( $post->ID, '_anime', true );
-    $all_anime = get_posts( array(
-        'post_type'   => 'anime',
-        'numberposts' => -1,
-        'orderby'     => 'post_title',
-        'order'       => 'ASC'
-    ));
-    ?>
-    <input type="hidden" name="anime_nonce" value="<?php echo wp_create_nonce( basename( __FILE__ ) ); ?>" />
-    <table class="form-table">
-        <tr valign="top">
-            <th scope="row">
-                <label for="anime">Anime</label>
-            </th>
-            <td>
-                <select name="anime">
-                    <?php foreach ( $all_anime as $anime ) : ?>
-                        <option value="<?php echo $anime->ID; ?>" <?php echo selected( $selected_anime, $anime->ID, false); ?>><?php echo $anime->post_title; ?></option>
-                    <?php endforeach; ?>
-                </select>
-            </td>
-        </tr>
-    </table>
-    <?php
+	global $post;
+	$selected_anime = get_post_meta( $post->ID, '_anime', true );
+	$all_anime = get_posts( array(
+		'post_type'   => 'anime',
+		'numberposts' => -1,
+		'orderby'     => 'post_title',
+		'order'       => 'ASC'
+	));
+	?>
+	<input type="hidden" name="anime_nonce" value="<?php echo wp_create_nonce( basename( __FILE__ ) ); ?>" />
+	<table class="form-table">
+		<tr valign="top">
+			<th scope="row">
+				<label for="anime"><?php _e( 'Anime', 'anva-anime-manager' ); ?></label>
+			</th>
+			<td>
+				<select name="anime">
+					<option value="">
+						<?php _e( 'Select the anime', 'anva-anime-manager' ); ?>
+					</option>
+					<?php foreach ( $all_anime as $anime ) : ?>
+						<option value="<?php echo $anime->ID; ?>" <?php echo selected( $selected_anime, $anime->ID, false); ?>>
+							<?php echo $anime->post_title; ?>
+						</option>
+					<?php endforeach; ?>
+				</select>
+			</td>
+		</tr>
+	</table>
+	<?php
 }
 
-add_action( 'save_post', 'save_anime_field' );
 function save_anime_field( $post_id ) {
 
-    // only run this for series
-    if ( 'anime_episode' != get_post_type( $post_id ) )
-        return $post_id;
+	// only run this for series
+	if ( 'anime_episode' != get_post_type( $post_id ) )
+		return $post_id;
 
-    // verify nonce
-    if ( empty( $_POST['anime_nonce'] ) || !wp_verify_nonce( $_POST['anime_nonce'], basename( __FILE__ ) ) )
-        return $post_id;
+	// verify nonce
+	if ( empty( $_POST['anime_nonce'] ) || !wp_verify_nonce( $_POST['anime_nonce'], basename( __FILE__ ) ) )
+		return $post_id;
 
-    // check autosave
-    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
-        return $post_id;
+	// check autosave
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
+		return $post_id;
 
-    // check permissions
-    if ( !current_user_can( 'edit_post', $post_id ) )
-        return $post_id;
+	// check permissions
+	if ( !current_user_can( 'edit_post', $post_id ) )
+		return $post_id;
 
-    // save
-    update_post_meta( $post_id, '_anime', $_POST['anime'] );
+	// save
+	update_post_meta( $post_id, '_anime', $_POST['anime'] );
 }
+add_action( 'save_post', 'save_anime_field' );
 
 /**
  * Create dropdown to filter episodes y anime.
@@ -63,38 +68,38 @@ function save_anime_field( $post_id ) {
  */
 function anime_admin_episodes_filter_restrict_manage_posts() {
 
-    $type = 'anime';
+	$type = 'anime';
 
-    if (isset($_GET['post_type'])) {
-        $type = $_GET['post_type'];
-    }
+	if (isset($_GET['post_type'])) {
+		$type = $_GET['post_type'];
+	}
 
-    //only add filter to post type you want
-    if ('anime_episode' == $type){
-        $all_anime = get_posts( array(
-            'post_type'   => 'anime',
-            'numberposts' => -1,
-            'orderby'     => 'post_title',
-            'order'       => 'ASC'
-        ));
-        ?>
-        <select name="anime_filter">
-        <option value="">
-            <?php _e( 'All Anime ', 'wose45436' ); ?>
-        </option>
-        <?php
-            $current = isset( $_GET['anime_filter'] ) ? $_GET['anime_filter'] : '';
-            foreach ( $all_anime as $anime ) {
-                printf( '<option value="%s" %s>%s</option>',
-                    $anime->ID,
-                    $anime->ID == $current ? ' selected="selected"' : '',
-                    $anime->post_title
-                );
-            }
-        ?>
-        </select>
-        <?php
-    }
+	//only add filter to post type you want
+	if ('anime_episode' == $type){
+		$all_anime = get_posts( array(
+			'post_type'   => 'anime',
+			'numberposts' => -1,
+			'orderby'     => 'post_title',
+			'order'       => 'ASC'
+		));
+		?>
+		<select name="anime_filter">
+		<option value="">
+			<?php _e( 'All Anime ', 'wose45436' ); ?>
+		</option>
+		<?php
+			$current = isset( $_GET['anime_filter'] ) ? $_GET['anime_filter'] : '';
+			foreach ( $all_anime as $anime ) {
+				printf( '<option value="%s" %s>%s</option>',
+					$anime->ID,
+					$anime->ID == $current ? ' selected="selected"' : '',
+					$anime->post_title
+				);
+			}
+		?>
+		</select>
+		<?php
+	}
 }
 add_action( 'restrict_manage_posts', 'anime_admin_episodes_filter_restrict_manage_posts' );
 
@@ -106,43 +111,43 @@ add_action( 'restrict_manage_posts', 'anime_admin_episodes_filter_restrict_manag
  * @return Void
  */
 function anime_admin_episodes_filter( $query ) {
-    global $pagenow;
-    $type = 'anime_episode';
-    
-    if (isset($_GET['post_type'])) {
-        $type = $_GET['post_type'];
-    }
-    
-    if ( 'anime_episode' == $type && is_admin() && $pagenow == 'edit.php' && isset( $_GET['anime_filter'] ) && $_GET['anime_filter'] != '' ) {
-        $query->query_vars['meta_key'] = '_anime';
-        $query->query_vars['meta_value'] = $_GET['anime_filter'];
-    }
+	global $pagenow;
+	$type = 'anime_episode';
+	
+	if (isset($_GET['post_type'])) {
+		$type = $_GET['post_type'];
+	}
+	
+	if ( 'anime_episode' == $type && is_admin() && $pagenow == 'edit.php' && isset( $_GET['anime_filter'] ) && $_GET['anime_filter'] != '' ) {
+		$query->query_vars['meta_key'] = '_anime';
+		$query->query_vars['meta_value'] = $_GET['anime_filter'];
+	}
 }
 add_filter( 'parse_query', 'anime_admin_episodes_filter' );
 
 
 function anime_post_type_link_filter( $post_link, $id = 0, $leavename = FALSE ) {
-    if ( strpos('%anime%', $post_link ) === 'FALSE' ) {
-        return $post_link;
-    }
+	if ( strpos('%anime%', $post_link ) === 'FALSE' ) {
+		return $post_link;
+	}
 
-    $post = get_post( $id );
-    
-    if ( ! is_object( $post ) || $post->post_type != 'anime_episode' ) {
-        return $post_link;
-    }
+	$post = get_post( $id );
+	
+	if ( ! is_object( $post ) || $post->post_type != 'anime_episode' ) {
+		return $post_link;
+	}
 
-    $episode_id = $post->ID;
-    $anime_id = get_post_meta( $post->ID, '_anime', true );
-    $anime    = get_post( $anime_id );
-    
-    if ( ! $anime_id ) {
-        $episode_slug = apply_filters( 'anva_anime_episode_slug', '%anime%/episode' );
-        return str_replace( $episode_slug, '', $post_link );
-    }
+	$episode_id = $post->ID;
+	$anime_id = get_post_meta( $post->ID, '_anime', true );
+	$anime    = get_post( $anime_id );
+	
+	if ( ! $anime_id ) {
+		$episode_slug = apply_filters( 'anva_anime_episode_slug', '%anime%/episode' );
+		return str_replace( $episode_slug, '', $post_link );
+	}
 
-    $anime_slug = $anime->post_name;
+	$anime_slug = $anime->post_name;
 
-    return str_replace( '%anime%', $anime_slug, $post_link );
+	return str_replace( '%anime%', $anime_slug, $post_link );
 }
 add_filter( 'post_type_link', 'anime_post_type_link_filter', 1, 3 );
